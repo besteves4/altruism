f973fd075a3b799ba587cba0b7cae7a1c35f6892
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2023-07-03 13:42:54 +0200

    test login functions

---
+++ functions.js
@@ -0,0 +1,34 @@
+import { login, getDefaultSession, handleIncomingRedirect } from '@inrupt/solid-client-authn-browser'
+
+window.onload=function(){
+    var example = document.getElementById('hello');
+    var h1 = document.createElement('h1');
+    h1.innerText = "hi";
+    example.appendChild(h1);
+}
+
+window.encrypt = () => {
+    console.log('logging')
+    loginToSelectedIdP();
+    handleRedirectAfterLogin();
+}
+
+function loginToSelectedIdP() {
+    const idp = document.getElementById("selectedIdP").value;
+    return login({
+        oidcIssuer: idp,
+        redirectUrl: new URL("/", window.location.href).toString(),
+        clientName: "My application"
+      });
+}
+
+// When redirected after login, finish the process by retrieving session information.
+async function handleRedirectAfterLogin() {
+    await handleIncomingRedirect(); // no-op if not part of login redirect
+  
+    const session = getDefaultSession();
+    if (session.info.isLoggedIn) {
+      // Update the page with the status.
+      document.getElementById("myWebID").value = session.info.webId;
+    }
+}
