4880ae14fc2464e06ed938f0752981389f7df87e
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2023-07-04 11:54:46 +0200

    update policy editor to generate altruistic policies

--- components/Editor.js
+++ components/Editor.js
@@ -22,12 +22,6 @@ import {
 import { RDF, ODRL } from "@inrupt/vocab-common-rdf";
 import { fetch } from "@inrupt/solid-client-authn-browser";
 
-/* import personalData from "../data/personaldata.json";
-import purpose from "../data/purposes.json"; */
-import getPaths from "../src/utils";
-
-const [personalData, purpose] = getPaths();
-
 async function getPolicyFilenames(policiesContainer) {
   console.log(policiesContainer);
   const myDataset = await getSolidDataset(policiesContainer.href, {
@@ -38,153 +32,75 @@ async function getPolicyFilenames(policiesContainer) {
   return policyList;
 }
 
-const policyTypes = [
-  { value: "permission", label: "Permission" },
-  { value: "prohibition", label: "Prohibition" },
+const altruisticPurpose = [
+  { value: "ScientificResearch", label: "Scientific Research" },
+  { value: "CombatClimateChange", label: "Combat Climate Change" },
+  { value: "ImproveHealthcare", label: "Improve Healthcare" },
+  { value: "ImprovePublicServices", label: "Improve Public Services" },
+  { value: "ImproveTransportMobility", label: "Improve Transport and Mobility" },
+  { value: "ProvideOfficialStatistics", label: "Provide Official Statistics" },
+  { value: "PublicPolicyMaking", label: "Public Policy Making" },
+];
+
+const dataTypes = [
+  { value: "Location", label: "Location" },
+  { value: "Health", label: "Health" },
 ];
 
 export function Editor() {
   const { session, sessionRequestInProgress } = useSession();
 
-  const [chosenPolicy, setChosenPolicy] = useState(policyTypes[0].value);
-  const [policyFilename, setPolicyFilename] = useState("example-policy.ttl");
-
-  // TODO: Move to src/utils.js or something:
-  /*   const assignObjectPaths = (obj, stack) => {
-    Object.keys(obj).forEach((k) => {
-      const node = obj[k];
-      if (typeof node === "object") {
-        node.path = stack ? `${stack}.${k}` : k;
-        assignObjectPaths(node, node.path);
-      }
-    });
-  }; */
-
-  // TODO: Only run this once, these don't need to be part of the render loop and will slow your application:
-  /*   assignObjectPaths(personalData);
-  assignObjectPaths(purpose); */
-
-  let selectedPD = [];
-  const handlePersonalData = (currentNode, selectedNodes) => {
-    for (var i = 0; i < selectedNodes.length; i++) {
-      //var value = selectedNodes[i].value;
-      var label = selectedNodes[i].label;
-      if (!selectedPD.includes(label)) {
-        selectedPD.push(label);
-      }
-    }
-    console.log(selectedPD);
-  };
-
-  let selectedPurpose = [];
-  const handlePurpose = (currentNode, selectedNodes) => {
-    for (var i = 0; i < selectedNodes.length; i++) {
-      //var value = selectedNodes[i].value;
-      var label = selectedNodes[i].label;
-      if (!selectedPurpose.includes(label)) {
-        selectedPurpose.push(label);
-      }
-    }
-    console.log(selectedPurpose);
-  };
-
-  // TODO: lift up, see what I did with policyTypes
-  const access = [{ label: "Read" }, { label: "Write" }, { label: "Append" }];
-
-  // You may wish to use a useState or useReducer for this:
-  // https://dev.to/colocodes/2-use-cases-of-the-usereducer-reactjs-hook-ine
-  let selectedAccess = [];
-  const handleAccess = (currentNode, selectedNodes) => {
-    for (var i = 0; i < selectedNodes.length; i++) {
-      //var value = selectedNodes[i].value;
-      var label = selectedNodes[i].label;
-      if (!selectedAccess.includes(label)) {
-        selectedAccess.push(label);
-      }
-    }
-    console.log(selectedAccess);
-  };
+  const [chosenPurpose, setChosenPurpose] = useState(altruisticPurpose[0].value);
+  const [chosenData, setChosenData] = useState(dataTypes[0].value);
+  const [dataStorage, setDataStorage] = useState("path/to/data.ttl");
+  const [policyStorage, setPolicyStorage] = useState("policy.ttl");
 
   const [display, setDisplay] = useState(false);
   const [displayResource, setDisplayResource] = useState("");
-  const [displayPolicyType, setDisplayPolicyType] = useState("");
-  const [displayAccess, setDisplayAccess] = useState([]);
-  const [displayPD, setDisplayPD] = useState([]);
-  const [displayPurposeOperator, setDisplayPurposeOperator] = useState();
-  const [displayPurpose, setDisplayPurpose] = useState([]);
+  const [displayPolicy, setDisplayPolicy] = useState("");
+  const [displayPurpose, setDisplayPurpose] = useState("");
 
   const generatePolicy = () => {
     // TODO: chosenPolicy/selectedPD/selectedPurpose have to be gathered only when generatePolicy is activated
     let newPolicy = createSolidDataset();
 
+    const dga = "http://anon/dgaterms#"
     const dpv = "http://www.w3.org/ns/dpv#";
-    const odrl = "http://www.w3.org/ns/odrl/2/";
-    const oac = "https://w3id.org/oac/";
-
-    const oacPurpose = `${oac}Purpose`;
-    const odrlPolicyType = `${odrl}${chosenPolicy}`;
+    const oac = "https://w3id.org/oac#";
 
     let policy = createThing({ name: "policy1" });
-    let policyType = createThing({ name: chosenPolicy + "1" });
-    policy = addUrl(policy, RDF.type, ODRL.Policy);
-    policy = addUrl(policy, odrlPolicyType, policyType);
+    let permission = createThing({ name: "permission1" });
+    policy = addUrl(policy, RDF.type, ODRL.Offer);
+    policy = addUrl(policy, ODRL.permission, permission);
     newPolicy = setThing(newPolicy, policy);
 
+    permission = addUrl(permission, ODRL.target, dataStorage);
+    permission = addUrl(permission, ODRL.action, `${oac}Read`);
+    permission = addUrl(permission, ODRL.assigner, session.info.webId);
     let purposeConstraint = createThing({ name: "purposeConstraint" });
+    permission = addUrl(permission, ODRL.constraint, purposeConstraint);
+    newPolicy = setThing(newPolicy, permission);
 
-    for (let i = 0; i < selectedPD.length; i++) {
-      var pd = selectedPD[i];
-      policyType = addUrl(policyType, ODRL.target, `${oac}${pd}`);
-    }
-
-    for (let i = 0; i < selectedAccess.length; i++) {
-      var acc = selectedAccess[i];
-      policyType = addUrl(policyType, ODRL.action, `${oac}${acc}`);
-    }
-
-    policyType = addUrl(policyType, ODRL.assigner, session.info.webId);
-    policyType = addUrl(policyType, ODRL.constraint, purposeConstraint);
-    newPolicy = setThing(newPolicy, policyType);
-
-    purposeConstraint = addUrl(purposeConstraint, ODRL.leftOperand, oacPurpose);
-
-    let purposeOperator = "";
-    if (selectedPurpose.length > 1) {
-      purposeOperator = ODRL.isAnyOf;
-    } else {
-      purposeOperator = ODRL.isA;
-    }
-    purposeConstraint = addUrl(
-      purposeConstraint,
-      ODRL.operator,
-      purposeOperator
-    );
-
-    for (var p = 0; p < selectedPurpose.length; p++) {
-      var purp = selectedPurpose[p].replaceAll(" ", "");
-      purposeConstraint = addUrl(
-        purposeConstraint,
-        ODRL.rightOperand,
-        `${dpv}${purp}`
-      );
-    }
-
+    purposeConstraint = addUrl(purposeConstraint, ODRL.leftOperand, `${oac}Purpose`);
+    purposeConstraint = addUrl(purposeConstraint, ODRL.operator, ODRL.isA);
+    purposeConstraint = addUrl(purposeConstraint, ODRL.rightOperand, `${dga}${chosenPurpose}`);
     newPolicy = setThing(newPolicy, purposeConstraint);
 
     getPodUrlAll(session.info.webId).then((response) => {
-      if (chosenPolicy === "") {
-        alert("Choose a type of policy");
-      } else if (selectedPD.length < 1) {
-        alert("Choose the categories of personal data of the policy");
-      } else if (selectedPurpose.length < 1) {
-        alert("Choose the purpose of the policy");
-      } else if (selectedAccess.length < 1) {
-        alert("Choose the access modes applicable to the policy");
+      if (chosenPurpose === "") {
+        alert("Choose the purpose of policy");
+      } else if (chosenData === "") {
+        alert("Choose the categories of personal data available in the resource");
+      } else if (dataStorage === "") {
+        alert("Indicate the URL of the resource");
+      } else if (policyStorage === "") {
+        alert("Choose a name for the file storing the policy");
       } else {
         const podRoot = response[0];
-        const policiesContainer = "private/odrl_policies/";
+        // TODO: check if ```policiesContainer``` exists before using it as a path to store policies
+        const policiesContainer = "private/altruism/";
         const podPoliciesContainer = new URL(policiesContainer, podRoot);
-        const filename = policyFilename;
+        const filename = policyStorage;
 
         const filenameContainer = `${policiesContainer}${filename}`;
         const filenameSave = new URL(filenameContainer, podRoot);
@@ -197,14 +113,9 @@ export function Editor() {
               saveSolidDatasetAt(filenameSave.href, newPolicy, {
                 fetch: fetch,
               });
-              setDisplayPolicyType(chosenPolicy);
-              setDisplayResource(filenameSave.href);
-              setDisplayAccess(selectedAccess.map((a) => `oac:${a}`));
-              setDisplayPD(selectedPD.map((pd) => `oac:${pd}`));
-              setDisplayPurposeOperator(purposeOperator.split("/").pop());
-              setDisplayPurpose(
-                selectedPurpose.map((p) => `dpv:${p.replaceAll(" ", "")}`)
-              );
+              setDisplayPolicy(filenameSave.href);
+              setDisplayResource(dataStorage);
+              setDisplayPurpose(chosenPurpose);
               setDisplay(true);
             } catch (error) {
               console.log(error);
@@ -225,23 +136,35 @@ export function Editor() {
         <div className="container">
           <div className="">
             <p>
-              <b>Choose type of policy:</b>
+              <b>Indicate the URL of the resource to share:</b>
+            </p>
+            <TextField
+              size="small"
+              onChange={(ev) => setDataStorage(ev.target.value)}
+              value={dataStorage}
+            />
+          </div>
+        </div>
+        <div className="container">
+          <div className="">
+            <p>
+              <b>Choose type of data present in the resource:</b>
             </p>
             <FormControl fullWidth>
               <InputLabel id="policy-type-label" htmlFor="policy-type-select">
-                Policy Type
+                Data Type
               </InputLabel>
               <Select
                 size="small"
                 labelId="policy-type-label"
                 id="policy-type-select"
-                label="Policy Type"
-                onChange={(ev) => setChosenPolicy(ev.target.value)}
-                value={chosenPolicy}
+                label="Data Type"
+                onChange={(ev) => setChosenData(ev.target.value)}
+                value={chosenData}
               >
-                {policyTypes.map((policy) => (
-                  <MenuItem key={policy.value} value={policy.value}>
-                    {policy.label}
+                {dataTypes.map((data) => (
+                  <MenuItem key={data.value} value={data.value}>
+                    {data.label}
                   </MenuItem>
                 ))}
               </Select>
@@ -251,37 +174,27 @@ export function Editor() {
         <div className="container">
           <div className="">
             <p>
-              <b>Choose type of personal data:</b>
+              <b>Choose purpose for altruistic reuse:</b>
             </p>
-            <DropdownTreeSelect
-              data={personalData}
-              onChange={handlePersonalData}
-              className="tree-select"
-            />
-          </div>
-        </div>
-        <div className="container">
-          <div className="">
-            <p>
-              <b>Choose purpose:</b>
-            </p>
-            <DropdownTreeSelect
-              data={purpose}
-              onChange={handlePurpose}
-              className="tree-select"
-            />
-          </div>
-        </div>
-        <div className="container">
-          <div className="">
-            <p>
-              <b>Choose applicable access modes:</b>
-            </p>
-            <DropdownTreeSelect
-              data={access}
-              onChange={handleAccess}
-              className="tree-select"
-            />
+            <FormControl fullWidth>
+              <InputLabel id="policy-type-label" htmlFor="policy-type-select">
+                Purpose
+              </InputLabel>
+              <Select
+                size="small"
+                labelId="policy-type-label"
+                id="policy-type-select"
+                label="Purpose"
+                onChange={(ev) => setChosenPurpose(ev.target.value)}
+                value={chosenPurpose}
+              >
+                {altruisticPurpose.map((purpose) => (
+                  <MenuItem key={purpose.value} value={purpose.value}>
+                    {purpose.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </div>
         </div>
         <div className="container">
@@ -291,8 +204,8 @@ export function Editor() {
             </p>
             <TextField
               size="small"
-              onChange={(ev) => setPolicyFilename(ev.target.value)}
-              value={policyFilename}
+              onChange={(ev) => setPolicyStorage(ev.target.value)}
+              value={policyStorage}
             />
           </div>
           <div className="bottom-container">
@@ -305,22 +218,22 @@ export function Editor() {
       <div className="right-col">
         {display && (
           <pre>{`
-            PREFIX odrl: <http://www.w3.org/ns/odrl/2/>
-            PREFIX oac: <https://w3id.org/oac/>
-            PREFIX dpv: <http://www.w3.org/ns/dpv#>
             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX odrl: <http://www.w3.org/ns/odrl/2/>
+            PREFIX oac: <${oac}>
+            PREFIX dga: <${dga}>            
 
-            <${displayResource}>
-                rdf:type odrl:Policy ;
+            <${displayPolicy}>
+                rdf:type odrl:Offer ;
                 odrl:profile oac: ;
-                odrl:${displayPolicyType} [
+                odrl:permission [
                     odrl:assigner <${session.info.webId}> ;
-                    odrl:action ${displayAccess} ;
-                    odrl:target ${displayPD} ;
+                    odrl:action oac:Read ;
+                    odrl:target ${displayResource} ;
                     odrl:constraint [
                         odrl:leftOperand oac:Purpose ;
-                        odrl:operator odrl:${displayPurposeOperator} ;
-                        odrl:rightOperand ${displayPurpose}
+                        odrl:operator odrl:isA ;
+                        odrl:rightOperand dga:${displayPurpose}
                     ]
                 ] .
           `}</pre>
